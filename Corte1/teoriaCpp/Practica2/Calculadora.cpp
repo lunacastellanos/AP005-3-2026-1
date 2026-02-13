08e626271806c4be7e289976797535c561a3758a
lunacastellanos <lpcastellanoss@udistrital.edu.co>
2026-02-13 12:43:33 -0500

    Add files via upload

---
+++ Corte1/teoriaCpp/Practica2/Calculadora.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int main() {
+    int n1, n2, suma;
+
+    std::cout << "Introduce el primer numero: ";
+    std::cin >> n1;
+
+    std::cout << "Introduce el segundo numero: ";
+    std::cin >> n2;
+
+  
+    suma = n1 + n2;
+
+    std::cout << "La suma es: " << suma << std::endl;
+
+    return 0;
+}
+
