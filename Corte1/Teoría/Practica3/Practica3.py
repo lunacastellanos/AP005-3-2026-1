2ac2325a50aa6c4c64d45aeb102fa96430b063d9
lunacastellanos <lpcastellanoss@udistrital.edu.co>
2026-03-23 20:09:14 -0500

    Add files via upload

---
+++ Corte1/Teoría/Practica3/Practica3.py
@@ -0,0 +1,17 @@
+while True:
+    a = int(input("Ingrese 1 para verificar un número o 0 para salir: "))
+
+    if a == 1:
+        numero = int(input("Ingrese un número: "))
+
+        if numero % 2 == 0:
+            print("El número es PAR")
+        else:
+            print("El número es IMPAR")
+
+    elif a == 0:
+        print("Saliendo del programa...")
+        break  # saca del while
+
+    else:
+        print("Opción inválida, intente de nuevo")
