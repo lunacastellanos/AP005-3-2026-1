588a9083578af84ba8d927ae072189e99a164aa9
lunacastellanos <lpcastellanoss@udistrital.edu.co>
2026-03-23 19:54:37 -0500

    Update variable values and print statements in practica2.py

--- Corte1/Teoría/Practica2/practica2.py
+++ Corte1/Teoría/Practica2/practica2.py
@@ -1,10 +1,12 @@
-n1=3
-n2=3.0
-n3="3"
-print()
-print("n1 es: ")
-print(type(n1))
-print("n2 es: ")
-print(type(n2))
-print("n3 es: ")
-print(type(n3))
+n1 = 4
+n2 = 2.6
+n3 = "3"
+
+print("n1 es:", n1)
+print("tipo de n1:", type(n1))
+
+print("n2 es:", n2)
+print("tipo de n2:", type(n2))
+
+print("n3 es:", n3)
+print("tipo de n3:", type(n3))
